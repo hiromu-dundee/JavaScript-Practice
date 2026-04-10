f12975617cfa1da1c2a77e078fa656cde521ee1d
takama <takama011711@gmail.com>
2026-04-11 00:34:07 +0900

    Update ToDO to ToDo-Content required.

--- 1-Todo-App/js/script.js
+++ 1-Todo-App/js/script.js
@@ -1,50 +1,59 @@
-const addBtn = document.querySelector('#add-btn');
-const content = document.querySelector('#content');
-const contentList = document.querySelector('#todo-list');
+const addBtn = document.querySelector("#add-btn");
+const content = document.querySelector("#content");
+const contentList = document.querySelector("#todo-list");
+const ErrorMessage = document.querySelector(".Error-Message");
 
 let todos = [];
 
-addBtn.addEventListener('click', () => {
-    todos.push({todoContent:content.value, isEditing:false});
+addBtn.addEventListener("click", () => {
+  ErrorMessage.textContent = "";
+  if (!content.value.trim()) {
+    ErrorMessage.textContent = "Error : ToDo Content is required.";
+  } else {
+    todos.push({ todoContent: content?.value, isEditing: false });
     render();
+  }
 });
 
-contentList.addEventListener('click', (e) => {
-    if (e.target.classList.contains('edit-complete')) {
-        const parent = e.target.closest('.todo');
+contentList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("edit-complete")) {
+    const parent = e.target.closest(".todo");
 
-        todos[e.target.dataset.index].todoContent = parent.querySelector(".editTodo").value;
-        todos[e.target.dataset.index].isEditing = false;
-        render();
-
-    } else if (e.target.classList.contains('edit')) {
-        todos[e.target.dataset.index].isEditing = true;
-        render();
-
-    } else if (e.target.classList.contains('delete')) {
-        todos.splice(e.target.dataset.index, 1);
-        render();
-    }
+    todos[e.target.dataset.index].todoContent =
+      parent.querySelector(".editTodo").value;
+    todos[e.target.dataset.index].isEditing = false;
+    render();
+  } else if (e.target.classList.contains("edit")) {
+    todos[e.target.dataset.index].isEditing = true;
+    render();
+  } else if (e.target.classList.contains("delete")) {
+    todos.splice(e.target.dataset.index, 1);
+    render();
+  }
 });
 
 function render() {
-    contentList.innerHTML = "";
-    content.value = "";
+  contentList.innerHTML = "";
+  content.value = "";
 
-    todos.forEach((todo, index) => {
-        const todoContent = `
+  todos.forEach((todo, index) => {
+    const todoContent = `
         <li>
             <div class="todo">
                 <h2 class="todo-number">No.${index + 1}</h2>
-                ${todo.isEditing ? `
+                ${
+                  todo.isEditing
+                    ? `
                 <input type="text" class="editTodo" placeholder="Edit ToDo" value="${todo.todoContent}" required><input class="edit-complete" type="submit" value="OK" data-index="${index}">
-                ` : `<p class="todo-content">${todo.todoContent}</p>`}
+                `
+                    : `<p class="todo-content">${todo.todoContent}</p>`
+                }
                 <button class="delete" data-index="${index}">Delete</button>
                 ${todo.isEditing ? "" : `<button class="edit" data-index="${index}">Edit</button>`}
 
             </div>
         </li>`;
 
-        contentList.insertAdjacentHTML('beforeend', todoContent);
-    });
+    contentList.insertAdjacentHTML("beforeend", todoContent);
+  });
 }
--- 3-WeatherAPIApp/js/script.js
+++ 3-WeatherAPIApp/js/script.js
@@ -1,86 +1,98 @@
-const locationName = document.querySelector('.location-name');
-const weatherImage = document.querySelector('.weatherImage');
-const weatherTelop = document.querySelector('.telop');
-const temperature = document.querySelector('#temperature');
+const locationName = document.querySelector(".location-name");
+const weatherImage = document.querySelector(".weatherImage");
+const weatherTelop = document.querySelector(".telop");
+const temperature = document.querySelector("#temperature");
 
 const setBackgroundImage = function (weatherCode) {
-    if (weatherCode <= 1) {
-        document.body.style.backgroundImage = "url('images/sunny.jpg')";
-    } else if (weatherCode <= 3) {
-        document.body.style.backgroundImage = "url('images/cloudy.jpg')";
-    } else {
-        document.body.style.backgroundImage = "url('images/rainy.jpg')";
-    }
-}
+  if (weatherCode <= 1) {
+    document.body.style.backgroundImage = "url('images/sunny.jpg')";
+  } else if (weatherCode <= 3) {
+    document.body.style.backgroundImage = "url('images/cloudy.jpg')";
+  } else {
+    document.body.style.backgroundImage = "url('images/rainy.jpg')";
+  }
+};
 
 const Weather = function (weatherCode) {
-    if (weatherCode <= 1) {
-        return ['images/tennki-illust2.png', '晴れ'];
-    } else if ((61 <= weatherCode && weatherCode <= 65) || (80 <= weatherCode && weatherCode <= 82)) {
-        return ['images/tennki-illust7.png', '雨'];
-    } else if ((weatherCode == 2)) {
-        return ['images/tennki-illust14.png', '曇りのち晴れ'];
-    } else if (weatherCode == 3) {
-        return ['images/tennki-illust5.png', '曇り'];
-    }
-}
+  if (weatherCode <= 1) {
+    return ["images/tennki-illust2.png", "晴れ"];
+  } else if (
+    (61 <= weatherCode && weatherCode <= 65) ||
+    (80 <= weatherCode && weatherCode <= 82)
+  ) {
+    return ["images/tennki-illust7.png", "雨"];
+  } else if (weatherCode == 2) {
+    return ["images/tennki-illust14.png", "曇りのち晴れ"];
+  } else if (weatherCode == 3) {
+    return ["images/tennki-illust5.png", "曇り"];
+  }
+};
 
 const calculateDay = function (dayCode) {
-    switch (dayCode) {
-        case 0:
-            return '日';
-        case 1:
-            return '月';
-        case 2:
-            return '火';
-        case 3:
-            return '水';
-        case 4:
-            return '木';
-        case 5:
-            return '金';
-        case 6:
-            return '土';
-    }
-}
+  switch (dayCode) {
+    case 0:
+      return "日";
+    case 1:
+      return "月";
+    case 2:
+      return "火";
+    case 3:
+      return "水";
+    case 4:
+      return "木";
+    case 5:
+      return "金";
+    case 6:
+      return "土";
+  }
+};
 
-window.addEventListener('load', () => {
-    fetch('https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&daily=weathercode,precipitation_sum,temperature_2m_max,temperature_2m_min&current=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=Asia%2FTokyo')
-        .then((response) => response.json())
-        .then((forecasts) => {
-            console.log(forecasts);
-            locationName.textContent = forecasts.timezone;
-            console.log(forecasts.daily.weathercode[0]);
+window.addEventListener("load", () => {
+  fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&daily=weathercode,precipitation_sum,temperature_2m_max,temperature_2m_min&current=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=Asia%2FTokyo",
+  )
+    .then((response) => response.json())
+    .then((forecasts) => {
+      console.log(forecasts);
+      locationName.textContent = forecasts.timezone;
+      console.log(forecasts.daily.weathercode[0]);
 
-            const weatherCode = forecasts.daily.weathercode[0];
+      const weatherCode = forecasts.daily.weathercode[0];
 
-            [weatherImage.src, weatherTelop.textContent] = Weather(weatherCode);
-            setBackgroundImage(weatherCode);
-            console.log(weatherImage);
-            // weatherImage.src = 'images/tennki-illust5.png';
+      [weatherImage.src, weatherTelop.textContent] = Weather(weatherCode);
+      setBackgroundImage(weatherCode);
+      console.log(weatherImage);
+      // weatherImage.src = 'images/tennki-illust5.png';
 
-            document.querySelector('.humid').textContent = `湿度：${forecasts.current.relative_humidity_2m}%`;
-            document.querySelector('.wind').textContent = `風速：${forecasts.current.wind_speed_10m}m`;
+      document.querySelector(".humid").textContent =
+        `湿度：${forecasts.current.relative_humidity_2m}%`;
+      document.querySelector(".wind").textContent =
+        `風速：${forecasts.current.wind_speed_10m}m`;
 
-            temperature.textContent = forecasts.current.temperature_2m + "℃";
+      temperature.textContent = forecasts.current.temperature_2m + "℃";
 
-            const listItem = document.querySelector('.forecasts-display');
+      const listItem = document.querySelector(".forecasts-display");
 
-            listItem.innerHTML = "";
+      listItem.innerHTML = "";
 
-            for (let i = 1; i < forecasts.daily.time.length; i++) {
-                const weatherItem = document.createElement('li');
-                const [year, month, day] = forecasts.daily.time[i].split('-').map(Number);
-                const Day = new Date(year, month - 1, day);
-                const code = forecasts.daily.weathercode[i];
-                const [temp_max, temp_min] = [forecasts.daily.temperature_2m_max[i], forecasts.daily.temperature_2m_min[i]];
-                weatherItem.className = 'weatherItem';
-                const [img, telop] = Weather(code);
+      for (let i = 1; i < forecasts.daily.time.length; i++) {
+        const weatherItem = document.createElement("li");
+        const [year, month, day] = forecasts.daily.time[i]
+          .split("-")
+          .map(Number);
+        const Day = new Date(year, month - 1, day);
+        const code = forecasts.daily.weathercode[i];
+        const [temp_max, temp_min] = [
+          forecasts.daily.temperature_2m_max[i],
+          forecasts.daily.temperature_2m_min[i],
+        ];
+        weatherItem.className = "weatherItem";
+        const [img, telop] = Weather(code);
 
-                console.log(document.body.clientWidth);
+        console.log(document.body.clientWidth);
 
-                if (document.body.clientWidth < 800) {
-                    weatherItem.innerHTML = `
+        if (document.body.clientWidth < 800) {
+          weatherItem.innerHTML = `
                         <h2>${month}月${day}日(${calculateDay(Day.getDay())})</h2>
                         <div class="sub-list">
                             <img class="weatherImage-sub" src="${img}" alt="">
@@ -90,8 +102,8 @@ window.addEventListener('load', () => {
                             </div>
                         </div>
                     `;
-                } else {
-                    weatherItem.innerHTML = `
+        } else {
+          weatherItem.innerHTML = `
                         <h2>${month}月${day}日(${calculateDay(Day.getDay())})</h2>
                         <img class="weatherImage-sub" src="${img}" alt="">
                         <div class="temp">
@@ -99,13 +111,11 @@ window.addEventListener('load', () => {
                             <p class="sub-temp">最低：${temp_min}℃</p>
                         </div>
                     `;
-                }
-                listItem.appendChild(weatherItem);
-            }
-
-        })
-        .catch((error) => {
-            console.log('Error : ', error);
-        });
+        }
+        listItem.appendChild(weatherItem);
+      }
+    })
+    .catch((error) => {
+      console.log("Error : ", error);
+    });
 });
-
